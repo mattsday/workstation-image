c2cfecd61443728b1294196f533e6bde4b4a9c17
Matt Day <mattsday@gmail.com>
2024-11-13 08:10:21 +0000

    fix: home env

--- scripts/100_user_env.sh
+++ scripts/100_user_env.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+# Where is the user's home?
+USER_HOME="/home/user"
+
 # Change the shell to zsh
 chsh -s /bin/sh user
 
 # Clone & init the shell config if it doesn't exist
-if [ ! -d "${HOME}/.dotfiles" ]; then
-    sudo -u user git clone https://github.com/mattsday/dotfiles "${HOME}/.dotfiles" && sudo -u user "${HOME}/.dotfiles/init.sh"
+if [ ! -d "${USER_HOME}/.dotfiles" ]; then
+    sudo -u user git clone https://github.com/mattsday/dotfiles "${USER_HOME}/.dotfiles" && sudo -u user "${USER_HOME}/.dotfiles/init.sh"
 fi
